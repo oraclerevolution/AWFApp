3ccc14d5d0169bccc465e32ee644f77db3659118
oraclerevolution <mac.n.drix@gmail.com>
2019-07-12 13:27:58 +0200

    initial commit

---
+++ Components/NavigationDrawer.js
@@ -0,0 +1,42 @@
+import {createDrawerNavigator, createAppContainer} from 'react-navigation'
+import Ouverture from './Ouverture'
+import AfricaWebFestivalPage from './AfricaWebFestivalPage'
+import AfricaWeb2019 from './AfricaWeb2019'
+import ActualitesPage from './ActualitesPage'
+import AwfTipsPage from './AwfTipsPage'
+import AwfGame from './AwfGame'
+import AwfTheqPage from './AwfTheqPage'
+
+const MyDrawerNavigator = createDrawerNavigator({
+    'Accueil': {
+        screen: Ouverture,
+    },
+    'Africa Web Festival': {
+        screen: AfricaWebFestivalPage
+    },
+    'AWF 2019': {
+        screen: AfricaWeb2019
+    },
+    'Les actualités': {
+        screen: ActualitesPage
+    },
+    'AWF Tips': {
+        screen: AwfTipsPage
+    },
+    'AWF Fun': {
+        screen: AwfGame
+    },
+    'AWF Thèque': {
+        screen: AwfTheqPage
+    }
+
+
+
+
+    }, {
+    initialRouteName:'Accueil'
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
+export default MyApp
